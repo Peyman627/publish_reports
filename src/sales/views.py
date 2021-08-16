9ef55edec16eb07717b5b8f2c8e1320f775b9357
Peyman Hassani <hassani.peyman627@gmail.com>
2021-08-16 16:12:34 +0430

    panda data frame for sales added

--- src/sales/views.py
+++ src/sales/views.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from django.shortcuts import render
 from django.views import generic
 
@@ -7,7 +9,24 @@ from .forms import SalesSearchForm
 
 def home_view(request):
     form = SalesSearchForm(request.POST or None)
-    context = {'form': form}
+    sales_df = None
+
+    if request.method == 'POST':
+        date_from = request.POST['date_from']
+        date_to = request.POST['date_to']
+        chart_type = request.POST['chart_type']
+
+        qs = Sale.objects.filter(created__date__gte=date_from,
+                                 created__date__lte=date_to)
+        if len(qs) > 0:
+            sales_df = pd.DataFrame(qs.values())
+            sales_df_html = sales_df.to_html()
+        else:
+            pass
+    context = {
+        'form': form,
+        'sales_df': sales_df_html,
+    }
     return render(request, 'sales/home.html', context)
 
 
